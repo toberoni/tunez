35686918e86a0e141fc2c1920ca567acd7629ae2
toberoni <toberoni@mailbox.org>
2025-05-22 12:12:09 +0700

    chapter 3 - add sorting

--- lib/tunez/music/artist.ex
+++ lib/tunez/music/artist.ex
@@ -21,6 +21,7 @@ defmodule Tunez.Music.Artist do
      end 
 
       filter expr(contains(name, ^arg(:query)))
+      pagination offset?: true, default_limit: 12
     end
 
 
@@ -37,6 +38,7 @@ defmodule Tunez.Music.Artist do
 
     attribute :name, :string do
       allow_nil? false
+      public? true
     end
 
     attribute :biography, :string
@@ -45,8 +47,8 @@ defmodule Tunez.Music.Artist do
       default []
     end
 
-    create_timestamp :inserted_at
-    update_timestamp :updated_at
+    create_timestamp :inserted_at, public?: true
+    update_timestamp :updated_at, public?: true
   end
 
   relationships do
--- lib/tunez_web/live/artists/index_live.ex
+++ lib/tunez_web/live/artists/index_live.ex
@@ -13,14 +13,30 @@ defmodule TunezWeb.Artists.IndexLive do
 
   def handle_params(params, _url, socket) do
     query_text = Map.get(params, "q", "")
-    artists = Tunez.Music.search_artists!(query_text)
+    sort_by = Map.get(params, "sort_by") |> validate_sort_by()
+    page_params = AshPhoenix.LiveView.page_from_params(params, 12)
+
+    page = Tunez.Music.search_artists!(query_text, 
+      page: page_params,
+      query: [sort_input: sort_by])
 
     socket =
       socket
       |> assign(:query_text, query_text)
-      |> assign(:artists, artists)
+      |> assign(:page, page)
+      |> assign(:sort_by, sort_by)
 
     {:noreply, socket}
+  end
+
+  def handle_event("change-sort", %{"sort_by" => sort_by}, socket) do
+    params = remove_empty(%{q: socket.assigns.query_text, sort_by: sort_by})
+    {:noreply, push_patch(socket, to: ~p"/?#{params}")}
+  end
+
+  def handle_event("search", %{"query" => query}, socket) do
+    params = remove_empty(%{q: query})
+    {:noreply, push_patch(socket, to: ~p"/?#{params}")}
   end
 
   def render(assigns) do
@@ -28,6 +44,9 @@ defmodule TunezWeb.Artists.IndexLive do
     <Layouts.app {assigns}>
       <.header responsive={false}>
         <.h1>Artists</.h1>
+        <:action>
+          <.sort_changer selected={@sort_by} />
+        </:action>
         <:action>
           <.search_box query={@query_text} method="get" data-role="artist-search" phx-submit="search" />
         </:action>
@@ -38,16 +57,17 @@ defmodule TunezWeb.Artists.IndexLive do
         </:action>
       </.header>
 
-      <div :if={@artists == []} class="p-8 text-center">
+      <div :if={@page.results == []} class="p-8 text-center">
         <.icon name="hero-face-frown" class="w-32 h-32 bg-gray-300" />
         <br /> No artist data to display!
       </div>
 
       <ul class="gap-6 lg:gap-12 grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4">
-        <li :for={artist <- @artists}>
+        <li :for={artist <- @page.results}>
           <.artist_card artist={artist} />
         </li>
       </ul>
+      <.pagination_links page={@page} query_text={@query_text} sort_by={@sort_by} />
     </Layouts.app>
     """
   end
@@ -102,15 +122,33 @@ defmodule TunezWeb.Artists.IndexLive do
 
   def pagination_links(assigns) do
     ~H"""
-    <div class="flex justify-center pt-8 space-x-4">
-      <.button_link data-role="previous-page" kind="primary" inverse>
-        « Previous
+    <div
+      :if={AshPhoenix.LiveView.prev_page?(@page) || AshPhoenix.LiveView.next_page?(@page)}
+            class="flex justify-center pt-8 space-x-4">
+      <.button_link data-role="previous-page" kind="primary" inverse
+        patch={~p"/?#{query_string(@page, @query_text, @sort_by, "prev")}"}
+        disabled={!AshPhoenix.LiveView.prev_page?(@page)}
+        >
+      « Previous
       </.button_link>
-      <.button_link data-role="next-page" kind="primary" inverse>
-        Next »
+      <.button_link data-role="next-page" kind="primary" inverse
+      patch={~p"/?#{query_string(@page, @query_text, @sort_by, "next")}"}
+      disabled={!AshPhoenix.LiveView.next_page?(@page)}
+      >
+      Next »
       </.button_link>
     </div>
     """
+  end
+
+   def query_string(page, query_text, sort_by, which) do
+    case AshPhoenix.LiveView.page_link_params(page, which) do
+    :invalid -> []
+    list -> list
+    end
+    |> Keyword.put(:q, query_text)
+    |> Keyword.put(:sort_by, sort_by)
+    |> remove_empty()
   end
 
   attr :query, :string, default: ""
@@ -154,8 +192,8 @@ defmodule TunezWeb.Artists.IndexLive do
 
   defp sort_options do
     [
-      {"recently updated", "updated_at"},
-      {"recently added", "inserted_at"},
+      {"recently updated", "-updated_at"},
+      {"recently added", "-inserted_at"},
       {"name", "name"}
     ]
   end
@@ -174,15 +212,6 @@ defmodule TunezWeb.Artists.IndexLive do
     Enum.filter(params, fn {_key, val} -> val != "" end)
   end
 
-  def handle_event("change-sort", %{"sort_by" => sort_by}, socket) do
-    params = remove_empty(%{q: socket.assigns.query_text, sort_by: sort_by})
-    {:noreply, push_patch(socket, to: ~p"/?#{params}")}
-  end
-
-  def handle_event("search", %{"query" => query}, socket) do
-    params = remove_empty(%{q: query})
-    {:noreply, push_patch(socket, to: ~p"/?#{params}")}
-  end
 
   def round_count(number) do
     case number do
